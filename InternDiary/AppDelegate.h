6a7a21d3024533b14493171a18119f38cb0290ee
sakahara <sakahara@hatena.ne.jp>
2014-08-14 19:29:54 +0900

    initial commit

---
+++ InternDiary/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  InternDiary
+//
+//  Created by sakahara on 2014/08/14.
+//  Copyright (c) 2014年 Hatena Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ InternDiary/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  InternDiary
+//
+//  Created by sakahara on 2014/08/14.
+//  Copyright (c) 2014年 Hatena Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
